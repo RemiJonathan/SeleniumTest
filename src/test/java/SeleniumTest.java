120c4118fefb12002c4e8ecdcfe51369c77c6080
Rémi Jonathan Choquette <contact@remijonathan.com>
2019-10-18 12:53:12 -0400

    Initial commit

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,66 @@
+import io.github.bonigarcia.seljup.SeleniumExtension;
+//import org.junit.Test;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.junit.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SeleniumExtension.class)
+class SeleniumTest {
+    private final String PATH = "C:\\Users\\conta\\Documents\\School\\SOFTWARE TESTING AND QUALITY ASSURANCE\\ChromeDriver";
+    private final String BROWSER = "webdriver.chrome.driver";
+    private final String SCREENSHOTS = "C:\\Users\\conta\\Documents\\School\\SOFTWARE TESTING AND QUALITY ASSURANCE\\Screenshots";
+
+    ChromeDriver driver;
+
+    public SeleniumTest(ChromeDriver driver){
+        this.driver = driver;
+        System.setProperty(BROWSER, PATH);
+    }
+
+    @Test
+    public void test_facebook_maximize(){
+        //Open up the webpage
+        driver.get("https://www.facebook.com");
+
+        //Maximize the webpage
+        driver.manage().window().maximize();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        driver.quit();
+
+    }
+
+    @Test
+    public void test_facebook_find_postbar(){
+        //Open up the webpage
+        driver.get("https://www.facebook.com");
+
+        //Maximize the webpage
+        driver.manage().window().maximize();
+
+        WebElement fbLogo = driver.findElement(By.className("fb_logo"));
+
+        assertThat(fbLogo.isDisplayed(), is(true));
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        driver.quit();
+
+    }
+}
