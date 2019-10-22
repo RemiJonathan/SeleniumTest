83f9271d54a7c14a94ee3d235672445781aef519
Rémi Jonathan Choquette <contact@remijonathan.com>
2019-10-22 13:58:01 -0400

    Test 2 of 5

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Date;
+import java.util.List;
+
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.junit.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,72 +17,32 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(SeleniumExtension.class)
 class SeleniumTest {
     //For School Computer use me
-    //private final String PATH = "C:\\Users\\conta\\Documents\\School\\SOFTWARE TESTING AND QUALITY ASSURANCE\\ChromeDriver";
+    private final String PATH = "C:\\Users\\conta\\Documents\\School\\SOFTWARE TESTING AND QUALITY ASSURANCE\\ChromeDriver";
 
     //For Home Computer use me
-    private final String PATH ="C:\\Users\\Remi\\Documents\\School\\SOFTWARE TESTING AND QUALITY ASSURANCE\\ChromeDriver";
+    //private final String PATH ="C:\\Users\\Remi\\Documents\\School\\SOFTWARE TESTING AND QUALITY ASSURANCE\\ChromeDriver";
 
     private final String BROWSER = "webdriver.chrome.driver";
 
     //For School Computer use me
-    //private final String SCREENSHOTS = "C:\\Users\\conta\\Documents\\School\\SOFTWARE TESTING AND QUALITY ASSURANCE\\Screenshots";
+    private final String SCREENSHOTS = "C:\\Users\\conta\\Documents\\School\\SOFTWARE TESTING AND QUALITY ASSURANCE\\Screenshots";
 
     //For Home Computer use me
-    private final String SCREENSHOTS = "C:\\Users\\Remi\\Documents\\School\\SOFTWARE TESTING AND QUALITY ASSURANCE\\Screenshots";
+    //private final String SCREENSHOTS = "C:\\Users\\Remi\\Documents\\School\\SOFTWARE TESTING AND QUALITY ASSURANCE\\Screenshots";
 
     ChromeDriver driver;
 
-    public SeleniumTest(ChromeDriver driver){
+    //User ID: mngr229914
+    String username = "mngr229914";
+    //Password: EpUjAzu
+    String password = "EpUjAzu";
+
+    public SeleniumTest(ChromeDriver driver) {
         this.driver = driver;
         System.setProperty(BROWSER, PATH);
     }
 
-    @Test
-    public void test_facebook_maximize(){
-        //Open up the webpage
-        driver.get("https://www.facebook.com");
-
-        //Maximize the webpage
-        driver.manage().window().maximize();
-
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        driver.quit();
-
-    }
-
-    @Test
-    public void test_facebook_find_postbar(){
-        //Open up the webpage
-        driver.get("https://www.facebook.com");
-
-        //Maximize the webpage
-        driver.manage().window().maximize();
-
-        WebElement fbLogo = driver.findElement(By.className("fb_logo"));
-
-        assertThat(fbLogo.isDisplayed(), is(true));
-
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        driver.quit();
-
-    }
-
-    @Test
-    public void testGuru99LoginAndFindElementOnPage(){
-        //User ID: mngr229914
-        String username = "mngr229914";
-        //Password: EpUjAzu
-        String password = "EpUjAzu";
+    public void login() {
         //Open the webpage
         driver.get("http://demo.guru99.com/V4/");
 
@@ -94,9 +57,87 @@ class SeleniumTest {
         //Find and click the login button
         WebElement loginButton = driver.findElement(By.name("btnLogin"));
         loginButton.click();
+    }
+
+    /**
+     * click on a tab
+     *
+     * @param tabId is the ID of the element in the nav bar
+     */
+    public void clickTab(int tabId) {
+        WebElement tab = driver.findElement(By.xpath("/html/body/div[3]/div/ul/li[" + tabId + "]/a"));
+        tab.click();
+    }
+
+    public void fillCustomerTable() {
+
+    }
+
+    @Test
+    public void testGuru99LoginAndFindElementOnPage() {
+        login();
 
         //Verify the successful login
         String managerStringValue = driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[3]/td")).getText();
-        assertEquals(managerStringValue,"Manger Id : "+username);
+        assertEquals(managerStringValue, "Manger Id : " + username);
+    }
+
+    @Test
+    public void test_registerNewCustomer_selectPage() {
+        login();
+        clickTab(2);
+        boolean addCustomerString = driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[1]/td/p")).isDisplayed();
+
+        assertEquals(addCustomerString, is(true));
+    }
+
+    @Test
+    public void test_registerNewCustomer_submitForm() {
+        login();
+        clickTab(2);
+
+        // Building Customer
+        String customerName;
+        String gender;
+        String dateOfBirth;
+        String address;
+        String city;
+        String state;
+        String personalIdentificationNumber;
+        String mobileNumber;
+        String email;
+        String password;
+
+        //Filling Customer info;
+        customerName = "Dylan Pita";
+        //gender = alpha male;
+        dateOfBirth = "0020000527";
+        address = "4997 Queens Bay";
+        city = "Balfour";
+        state = "BC";
+        personalIdentificationNumber = "568133";
+        mobileNumber = "5145552243";
+        email = "dylan@remyjonathan.com";
+        password = "Dylan224397";
+
+        driver.findElement(By.name("name")).sendKeys(customerName);
+        driver.findElements(By.name("rad1")).get(1).click();
+        driver.findElement(By.name("dob")).sendKeys(dateOfBirth);
+        driver.findElement(By.name("addr")).sendKeys(address);
+        driver.findElement(By.name("city")).sendKeys(city);
+        driver.findElement(By.name("state")).sendKeys(state);
+        driver.findElement(By.name("pinno")).sendKeys(personalIdentificationNumber);
+        driver.findElement(By.name("telephoneno")).sendKeys(mobileNumber);
+        driver.findElement(By.name("emailid")).sendKeys(email);
+        driver.findElement(By.name("password")).sendKeys(password);
+
+        driver.findElement(By.name("sub")).click();
+
+        assertEquals(driver.findElement(By.xpath("//*[@id=\"customer\"]/tbody/tr[1]/td/p")).getText(), "Customer Registered Successfully!!!");
+    }
+
+    @Test
+    public void test_returnToMgrDashboardAfterRegNewCustSucccess(){
+
     }
 }
